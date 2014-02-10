ec0eca9225b152143cbc674e5ffa3e252908d72e
dabootski <jasonbucki@gmail.com>
2014-02-10 01:09:46 -0600

    debug

--- app/models/event_processor.rb
+++ app/models/event_processor.rb
@@ -7,8 +7,16 @@ class EventProcessor
   def handle
     # Pass event off to handlers based on event name/title
     if @event.title == "Out of range"
+      puts "***************************"
+      puts "OUT OF RANGE HANDLER CALLED"
+      puts "***************************"
+
       DeviceOutOfRangeHandler.new.handle(@event)
     elsif @event.title == "In range"
+      puts "***************************"
+      puts "WITHIN RANGE HANDLER CALLED"
+      puts "***************************"
+
       DeviceWithinRangeHandler.new.handle(@event)
     end
   end
